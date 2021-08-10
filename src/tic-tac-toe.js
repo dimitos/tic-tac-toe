ac90f4be4567bc88d2c1d154f9a1de187f44259a
dimitos <kurort26@mail.ru>
2021-08-10 17:41:19 +0300

    feat: add getWinner() method

--- src/tic-tac-toe.js
+++ src/tic-tac-toe.js
@@ -23,7 +23,41 @@ class TicTacToe {
 
   // должен возвращать символ победителя ( x или o) или null, если победителя еще нет
   // проверяем есть ли победитель
-  getWinner() {}
+  getWinner() {
+    const arr = this.map;
+
+    arr.forEach((el) => {
+      if (el[0] === el[1] && el[0] === el[2] && el[0] !== null) return el[0];
+    });
+
+    for (let i = 0; i < 3; i++) {
+      if (
+        arr[0][i] === arr[1][i] &&
+        arr[0][i] === arr[2][i] &&
+        arr[0][i] !== null
+      ) {
+        return arr[0][i];
+      }
+    }
+
+    if (
+      arr[1][1] === arr[0][0] &&
+      arr[1][1] === arr[2][2] &&
+      arr[1][1] !== null
+    ) {
+      return arr[1][1];
+    }
+
+    if (
+      arr[1][1] === arr[0][2] &&
+      arr[1][1] === arr[2][0] &&
+      arr[1][1] !== null
+    ) {
+      return arr[1][1];
+    }
+
+    return null;
+  }
 
   //должен вернуть истину, если больше нет полей для размещения x или o, если больше нет ходов
   // ложь, если ходы есть
